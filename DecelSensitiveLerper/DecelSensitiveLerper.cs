f309106c37246b9ba5adb233c1fddbd833f1c8e7
fridgesrunning <am_ist@outlook.com>
2025-10-09 20:50:12 -0700

    DecelSensitiveLerper

--- DecelSensitiveLerper/DecelSensitiveLerper.cs
+++ DecelSensitiveLerper/DecelSensitiveLerper.cs
@@ -5,12 +5,12 @@ using OpenTabletDriver.Plugin.Output;
 using OpenTabletDriver.Plugin.Tablet;
 using OpenTabletDriver.Plugin.Timing;
 
-namespace Lerper
+namespace DecelSensitiveLerper
 {
-    [PluginName("Lerper")]
-    public class Lerper : AsyncPositionedPipelineElement<IDeviceReport>
+    [PluginName("DecelSensitiveLerper")]
+    public class DecelSensitiveLerper : AsyncPositionedPipelineElement<IDeviceReport>
     {
-        public Lerper() : base()
+        public DecelSensitiveLerper() : base()
         {
         }
 
@@ -25,7 +25,18 @@ namespace Lerper
                     reportMsAvg += ((consumeDelta - reportMsAvg) * 0.1f);
                     
                 lastPosition = currPosition;
+                currPosition = vec2IsFinite(currPosition) ? currPosition : report.Position;
                 currPosition = report.Position; 
+
+                lastVelocity = velocity;
+                velocity = (float)Math.Sqrt(Math.Pow(currPosition.X - lastPosition.X, 2) + Math.Pow(currPosition.Y - lastPosition.Y, 2));
+                accel = velocity - lastVelocity;
+
+                if (!(velocity == 0))
+                pow = Math.Clamp(1 + (accel / velocity), 0, 1);
+                else pow = 1;
+
+                Console.WriteLine(pow);
             }
             else OnEmit();
         }
@@ -37,13 +48,15 @@ namespace Lerper
             if (State is ITabletReport report && PenIsInRange())
             {
                 alpha = (float)Math.Clamp(alpha, 0, 1);
-                report.Position = Vector2.Lerp(lastPosition, currPosition, alpha);
+                report.Position = Vector2.Lerp(lastPosition, currPosition, (float)Math.Pow(alpha, pow));
                 State = report;
                 OnEmit();
             }
         }
 
+        private bool vec2IsFinite(Vector2 vec) => float.IsFinite(vec.X) & float.IsFinite(vec.Y);
         Vector2 currPosition, lastPosition;
+        float velocity, lastVelocity, accel, pow;
         private HPETDeltaStopwatch reportStopwatch = new HPETDeltaStopwatch();
         private float reportMsAvg = (1 / 300);
     }
