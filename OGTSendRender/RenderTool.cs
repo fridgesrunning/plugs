ae2e90df9a78aaea78563589f33c29e5ee7625fd
fridgesrunning <am_ist@outlook.com>
2025-11-13 18:05:17 -0800

    add OGT plugins

---
+++ OGTSendRender/RenderTool.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Numerics;
+using OpenTabletDriver.Plugin.Attributes;
+using OpenTabletDriver.Plugin.Output;
+using OpenTabletDriver.Plugin.Tablet;
+using OpenTabletDriver.Plugin.Timing;    
+
+/// Separate filter at the frequency of display hz that renders the image at a desired framerate.
+/// d tells the graph tool to draw at frequency when the pen is being applied.
+/// i polls for input at frequency and only draws when input is applied (mode change?).
+/// Data is handled by UpdateTool.
+
+namespace OGTRenderTool
+{
+    [PluginName("OGTRenderTool")]
+    public class RenderTool : AsyncPositionedPipelineElement<IDeviceReport>
+    {
+        public RenderTool() : base()
+        {
+        }
+
+        public override PipelinePosition Position => PipelinePosition.PreTransform;
+
+        [BooleanProperty("Wire (Hover Over This)", ""), DefaultPropertyValue(true), ToolTip
+        (
+            "Set this to true and frequency to 0 to only render on tablet update.\n" +
+            "Should probably set frequency to display refresh rate though, making setting this your decision."
+        )]
+        public bool Wire { get; set; };
+
+        protected override void ConsumeState()
+        {
+            if (State is ITabletReport report)
+            {
+                if (wire)
+                    UpdateState();
+            }
+            else OnEmit();
+        }
+
+        protected override void UpdateState()
+        {    
+            if (State is ITabletReport report && PenIsInRange())
+            {
+                Console.WriteLine("d");
+                State = report;
+                OnEmit();
+            }
+            else Console.WriteLine("i");
+        }
+    }
+}
---
+++ OGTSendUpdate/UpdateTool.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Numerics;
+using OpenTabletDriver.Plugin.Attributes;
+using OpenTabletDriver.Plugin.Output;
+using OpenTabletDriver.Plugin.Tablet;
+using OpenTabletDriver.Plugin.Timing;      
+
+/// This should probably always come before render tool in OTD filter order, but I'm unsure on whether it matters.
+/// Calculates some tablet metrics then sends them out, hopefully to be eaten up by OTDGraphTool.
+/// As you can see, the format seen on each report is (example)
+/// 
+/// v12.34567890
+/// a12.34567890
+/// j-12.34567890
+/// s-12.34567890
+/// x
+/// v6.7
+/// a-6.7
+/// j-6.7
+/// s6.7
+/// x
+/// 
+/// and so on. The graph tool takes numbers after v, a, j, s as data points for their respective v, a, j, s arrays.
+/// After this, the update sign x is seen, which makes the graph tool update the arrays with new data.
+/// t for "test" just tells the graph tool to assume it got new data. Lines are the result.
+
+namespace OGTUpdateTool
+{
+    [PluginName("OGTUpdateTool")]
+    public class UpdateTool : AsyncPositionedPipelineElement<IDeviceReport>
+    {
+        public UpdateTool() : base()
+        {
+        }
+
+        public override PipelinePosition Position => PipelinePosition.PreTransform;
+
+        [BooleanProperty("Wire (Hover Over This)", ""), DefaultPropertyValue(true), ToolTip
+        (
+            "Set this to true and frequency to 0 in most cases. Otherwise, prepare for a stress test if stress test is enabled."
+        )]
+        public bool Wire { get; set; };
+
+        [BooleanProperty("Stress Test (Hover Over This)", ""), DefaultPropertyValue(false), ToolTip
+        (
+            "Keep this at false unless you are testing if your CPU will be able to handle graphing a theoretical (frequency)Hz tablet."
+        )]
+        public bool Stress { get; set; };
+
+        protected override void ConsumeState()
+        {
+            if (State is ITabletReport report)
+            {
+                raw2Pos = raw1Pos;
+                raw1Pos = report.Position;
+                lastVelocity = velocity;
+                lastAccel = accel;
+                lastJerk = jerk;
+                velocity = Math.Sqrt(Math.Pow(raw1Pos.X - raw2Pos.X, 2) + Math.Pow(raw1Pos.Y - raw2Pos.Y, 2));
+                accel = velocity - lastVelocity;
+                jerk = accel - lastAccel;
+                snap = jerk - lastJerk;
+
+                Console.Write("v");
+                Console.WriteLine(velocity);
+                Console.Write("a");
+                Console.WriteLine(accel);
+                Console.Write("j");
+                Console.WriteLine(jerk);
+                Console.Write("s");
+                Console.WriteLine(snap);
+                Console.WriteLine("x");
+                
+                if (Wire)
+                    UpdateState();
+
+            }
+            else OnEmit();
+        }
+
+        protected override void UpdateState()
+        {
+            if (State is ITabletReport report && PenIsInRange())
+            {
+                if (Stress)
+                    Console.WriteLine("t");
+
+                State = report;
+                OnEmit();
+            }
+        }
+
+        public double velocity, accel, jerk, snap, lastVelocity, lastAccel, lastJerk, count;
+        public Vector2 raw1Pos, raw2Pos;
+
+    }
+}
